387e3fde951f528bb9690fa78d571eea926cafb3
JeroenBos <jer123bos@gmail.com>
2017-10-26 14:19:03 +0200

    First simple training algorithm

--- sin.py
+++ sin.py
@@ -1,4 +1,5 @@
 from math import sin, pi
+import keras
 
 INPUT_SIZE = 10
 DOMAIN_MAX = 2 * pi
@@ -6,6 +7,12 @@ DOMAIN_MAX = 2 * pi
 sin_input = [i * DOMAIN_MAX / INPUT_SIZE for i in range(INPUT_SIZE)]
 sin_output = [sin(x) for x in sin_input]
 
+model = keras.models.Sequential(layers=[
+    keras.layers.Dense(input_dim=1, units=1),
+    keras.layers.Activation(keras.activations.sigmoid),
+])
 
-for (a, b) in zip(sin_input, sin_output):
-    print(a, b)
+model.compile(optimizer=keras.optimizers.SGD(), loss='mse')
+
+model.fit(sin_input, sin_output, epochs=10)
+
