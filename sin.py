66ed1c634fbee0dfda47a044ed34ccc9b0acd38d
JeroenBos <jer123bos@gmail.com>
2017-10-26 14:20:24 +0200

    Simplify activation

--- sin.py
+++ sin.py
@@ -8,8 +8,7 @@ sin_input = [i * DOMAIN_MAX / INPUT_SIZE for i in range(INPUT_SIZE)]
 sin_output = [sin(x) for x in sin_input]
 
 model = keras.models.Sequential(layers=[
-    keras.layers.Dense(input_dim=1, units=1),
-    keras.layers.Activation(keras.activations.sigmoid),
+    keras.layers.Dense(input_dim=1, units=1, activation=keras.activations.sigmoid),
 ])
 
 model.compile(optimizer=keras.optimizers.SGD(), loss='mse')
