5c363438a18259e07fb3eb98602ab3d4a2e07135
JeroenBos <jer123bos@gmail.com>
2017-10-27 15:45:40 +0200

    Use shape from X

--- sin.py
+++ sin.py
@@ -1,15 +1,16 @@
 from math import sin, pi
 import tensorflow as tf
 import keras
+import numpy as np
 
 INPUT_SIZE = 10
 DOMAIN_MAX = 2 * pi
 
-sin_input = [i * DOMAIN_MAX / INPUT_SIZE for i in range(INPUT_SIZE)]
-sin_output = [sin(x) for x in sin_input]
+sin_input = np.array([[i * DOMAIN_MAX / INPUT_SIZE] for i in range(INPUT_SIZE)])
+sin_output = np.array([sin(x) for x in sin_input])
 
 model = keras.models.Sequential(layers=[
-    keras.layers.Dense(units=2, activation=keras.activations.sigmoid, input_dim=1, ),
+    keras.layers.Dense(units=2, activation=keras.activations.sigmoid, input_dim=sin_input.shape[1], ),
     keras.layers.Dense(units=1, activation=keras.activations.sigmoid),
 ])
 
