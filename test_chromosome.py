7ebec3f32e60d59d1cbb555b658a75386d252fe3
JeroenBos <jer123bos@gmail.com>
2017-11-10 14:05:53 +0100

    Implement test_crossover_result_is_between

--- test_chromosome.py
+++ test_chromosome.py
@@ -12,6 +12,17 @@ class ChromosomeTests(unittest.TestCase):
 
         assert allele == crossed_result
 
+    def test_crossover_result_is_between(self):
+        distribution = c.ParameterAllele.CollectionDistribution([10, 20, 50, 100])
+        allele1 = c.ParameterAllele(keras.layers.Dense, units=(10, distribution))
+        allele2 = c.ParameterAllele(keras.layers.Dense, units=(50, distribution))
+        possible_outcomes = [allele1, allele2, c.ParameterAllele(keras.layers.Dense, units=(20, distribution))]
+
+        for _ in range(20):
+            crossed_result = allele1.crossover(allele2)
+
+            assert crossed_result in possible_outcomes
+
 
 if __name__ == '__main__':
     unittest.main()
