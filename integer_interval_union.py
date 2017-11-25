f7de6c49efffa57b2e29ba4b3c454c482127a318
JeroenBos <jer123bos@gmail.com>
2017-11-25 12:59:31 +0100

    Make integer interval iterable

--- integer_interval_union.py
+++ integer_interval_union.py
@@ -4,6 +4,9 @@ from typing import *
 class IntegerInterval:
     empty = None
     def __init__(self, interval: Union[int, List[int], Tuple[int, int]]):
+        """
+        :param interval: A number, list, or range (by tuple, upperbound inclusive)
+        """
         assert isinstance(interval, (int, list, tuple))
         assert isinstance(interval, int) or all(isinstance(val, int) for val in iter(interval))
         assert isinstance(interval, (int, list)) or (len(interval) == 2 and interval[0] <= interval[1])
@@ -22,6 +25,14 @@ class IntegerInterval:
     @property
     def interval(self):
         return self.__interval
+
+    def __iter__(self):
+        if isinstance(self.interval, int):
+            return iter([self.interval])
+        if isinstance(self.interval, list):
+            return iter(self.interval)
+
+        return range(self.interval[0], self.interval[1] + 1)
 
 
 IntegerInterval.empty = IntegerInterval([])
